a2481ee662312c3af578d9552436ba643352dfb4
NikolaMinchevUOC <nminchev@uoc.edu>
2022-11-06 10:12:04 +0100

    Import fix

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import Inicio from "./src/components/Inicio";
-import Menu from "./src/components/Menu";
-
-
 
 export default function App() {
-  return (
-     <Inicio />
-     
-  );
+  return <Inicio />;
 }
-
-;
